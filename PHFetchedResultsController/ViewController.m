27a558f602020a8f9f5ec3fe6b808a20f4b25caa
1_am_a_geek <tmy0x3@icloud.com>
2015-11-30 18:51:37 +0900

    Initial Commit

---
+++ PHFetchedResultsController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PHFetchedResultsController
+//
+//  Created by 1amageek on 2015/11/30.
+//  Copyright © 2015年 Stamp inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ PHFetchedResultsController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PHFetchedResultsController
+//
+//  Created by 1amageek on 2015/11/30.
+//  Copyright © 2015年 Stamp inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
